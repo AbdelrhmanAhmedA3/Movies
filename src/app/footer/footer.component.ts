6c08a811b3940c48349a8a0d91401c9168484f65
AbdelrhmanAhmedA3 <abdelrhmanahmed9696@gmail.com>
2023-04-10 07:26:15 +0300

    deployment2

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { ShareDataService } from '../shareData/share-data.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+constructor(public shareDataService:ShareDataService){}
 }
